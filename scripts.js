0b89a93952582e711575efad098036af4533940a
4pes7ogether5trong <145257632+4pes7ogether5trong@users.noreply.github.com>
2023-09-17 06:38:16 -0500

    Update scripts.js
    Using Web Audio API

--- scripts.js
+++ scripts.js
@@ -1,66 +1,52 @@
-let audioCtx;
-let oscillator;
-let gainNode;
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const oscillator = audioCtx.createOscillator();
+const gainNode = audioCtx.createGain();
+
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+
+oscillator.type = 'sine';
+oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
+gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+oscillator.start();
+
+let interval;
 
 const startButton = document.getElementById('startMonitoring');
-const eegBar = document.getElementById('eegBar');
+const stopButton = document.getElementById('stopMonitoring');
 const refreshBar = document.getElementById('refreshBar');
 const roiBox = document.getElementById('roiBox');
-const baseFrequencyInput = document.getElementById('baseFrequency');
-const applySettingsButton = document.getElementById('applySettings');
 let barPosition = 0;
-
-let baseFrequency = 300; // This is a default value and can be adjusted
-
-document.getElementById("startSound").addEventListener("click", () => {
-    if (!audioCtx) {
-        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        oscillator = audioCtx.createOscillator();
-        gainNode = audioCtx.createGain();
-
-        oscillator.connect(gainNode);
-        gainNode.connect(audioCtx.destination);
-
-        oscillator.type = 'sine';
-        oscillator.frequency.setValueAtTime(baseFrequency, audioCtx.currentTime);
-        oscillator.start();
-    } else if (audioCtx.state === 'suspended') {
-        audioCtx.resume();
-    }
-});
-
-document.getElementById("stopSound").addEventListener("click", () => {
-    if (audioCtx && audioCtx.state !== 'suspended') {
-        audioCtx.suspend();
-    }
-});
 
 startButton.addEventListener('click', () => {
     gainNode.gain.setValueAtTime(1, audioCtx.currentTime); // Unmute sound
-    setInterval(moveRefreshBar, 100); // Moves the bar every 100ms
+    interval = setInterval(moveRefreshBar, 100); // Moves the bar every 100ms
 });
 
-applySettingsButton.addEventListener('click', () => {
-    const newBaseFrequency = parseInt(baseFrequencyInput.value);
-    oscillator.frequency.setValueAtTime(newBaseFrequency, audioCtx.currentTime);
+stopButton.addEventListener('click', () => {
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime); // Mute sound
+    clearInterval(interval);
 });
 
 function moveRefreshBar() {
-    barPosition += 2;
+    barPosition += 2;  // Moves 2% of the display width in each call
 
-    if (barPosition >= 100) {
+    if (barPosition >= 100) { // Resets after crossing the display
         barPosition = 0;
     }
 
     refreshBar.style.left = `${barPosition}%`;
-    const roiPosition = barPosition - 10;  
-    roiBox.style.left = `${roiPosition > 0 ? roiPosition : 0}%`;
 
-    const amplitude = (Math.sin(barPosition / 10) + 1) / 2;
+    // Make ROI Box follow the refresh bar
+    const roiPosition = barPosition - 10;  // Offset to have the ROI "chase" the bar
+    roiBox.style.left = `${roiPosition > 0 ? roiPosition : 0}%`; 
+
+    // Mock amplitude, returns value between 0 and 1
+    const amplitude = (Math.sin(barPosition / 10) + 1) / 2; 
     gainNode.gain.setValueAtTime(amplitude, audioCtx.currentTime);
 
+    // Change the pitch based on the frequency of the sine rhythm
     const derivative = Math.cos(barPosition / 10);
     const pitchFactor = 1 + derivative; 
-    oscillator.frequency.setValueAtTime(440 * pitchFactor, audioCtx.currentTime);
+    oscillator.frequency.setValueAtTime(440 * pitchFactor, audioCtx.currentTime); 
 }
-
